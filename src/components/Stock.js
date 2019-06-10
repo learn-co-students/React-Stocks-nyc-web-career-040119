a2d57f8a143c1fd1610b8ae068ff83a1a1d194fd
axionos <axionoso@gmail.com>
2019-06-10 17:46:01 -0400

    done.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,21 +1,25 @@
 import React from 'react'
 
-const Stock = () => (
-  <div>
 
-    <div className="card">
-      <div className="card-body">
-        <h5 className="card-title">{
-            //Company Name
-          }</h5>
-        <p className="card-text">{
-            //ticker: stock price
-          }</p>
+const Stock = (props) => {
+  console.log("Stock Props", props)
+
+  return(
+    <div>
+      <div className="card"
+       onClick={
+         // props.stockContainer === true ?
+         // ()=>props.addStock(props.stock) : ()=>props.removeStock(props.stock)
+         ()=>props.handleClick(props.stock)
+       }>
+        <div className="card-body">
+        <h5 className="card-title">{props.stock.name}</h5>
+        <p className="card-text">{props.stock.price}</p>
+        </div>
       </div>
     </div>
 
-
-  </div>
-);
+  )
+};
 
 export default Stock
